30fa500f9d506b34d62cd2ad61cc63385217408a
Shaine Hatch <hatch@fadelight.com>
2011-12-30 11:17:36 -0700

    Function skeleton

--- src/jquery.jhorizontal.js
+++ src/jquery.jhorizontal.js
@@ -1,5 +1,19 @@
 (function($) {
-  $.fn.jHorizontal = function() {
-    
+  $.fn.jHorizontal = function(arg) {
+    if(methods[arg]) {
+      return methods[arg].apply(this, Array.prototype.slice.call(arguments, 1));
+    }
+    else if (typeof method === 'object' || ! method) {
+      return methods.init.apply(this, arguments);
+    }
+    else {
+      $.error('Method ' +  arg + ' does not exist in jHorizontal.');
+    }
+  };
+  
+  var methods = {
+    init : function(options) { 
+      
+    }
   };
 })(jQuery);
